d60fb44b725c719f59f047eaa81588bb6c0a3f9c
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2020-03-14 19:11:29 +0100

    tested !

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ using namespace std;
 
 int main(){
 
+    Player beg(50,50,5,5,1.2,"beg");
+
+    string name = beg.getName();
+
+    int atk = beg.getCurrentAtk();
+
+    cout << name << " have " << atk << " atk" << endl;
+
 
 
 }
